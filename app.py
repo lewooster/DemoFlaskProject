5f104fe902bce1926e9011f147be574a384b972f
lewooster <lewis.wooster@datastax.com>
2020-02-22 20:32:45 -0500

    modified:   .idea/workspace.xml 	modified:   app.py

--- app.py
+++ app.py
@@ -204,31 +204,7 @@ def url_variables(name: str, age: int):
     """Example endpoint returning a response based on age
                 This is using docstrings for specifications.
                 ---
-                parameters:
-                  - name: palette
-                    in: path
-                    type: string
-                    enum: ['all', 'rgb', 'cmyk']
-                    required: true
-                    default: all
-                definitions:
-                  Palette:
-                    type: object
-                    properties:
-                      palette_name:
-                        type: array
-                        items:
-                          $ref: '#/definitions/Color'
-                  Color:
-                    type: string
-                responses:
-                  200:
-                    description: A list of colors (may be filtered by palette)
-                    schema:
-                      $ref: '#/definitions/Palette'
-                    examples:
-                      rgb: ['red', 'green', 'blue']
-                """
+    """
 
     if age < 18:
         return jsonify(message='Sorry '+name+', you are not old enough'), 401
